4ecba42095ae76b5c53078e9a8220f88976ba352
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-01 20:46:12 +0000

    Automatically backed up by Learn

---
+++ lib/introduction.rb
@@ -0,0 +1,12 @@
+def introduction(name)
+  
+  puts "Hi, my name is #{name}.\n"
+  
+end
+
+#Hi, my name is Dan and I am learning to program in Ember.js.\n"
+
+def introduction_with_language(name, language)
+
+introduction('Josh')
+introduction_with_language()
